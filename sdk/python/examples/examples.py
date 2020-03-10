db720e2e37d10dfd06e755f0f5c0cbfa65bcb791
bad-spuerman <xiangchao0105@163.com>
2020-03-10 20:21:54 +0800

    init

--- sdk/python/examples/examples.py
+++ sdk/python/examples/examples.py
@@ -1,9 +1,6 @@
 import logging
 import time
 
-import sys
-sys.path.append('..')
-
 from broker.client import BrokerClient
 
 if __name__ == '__main__':
@@ -17,7 +14,7 @@ if __name__ == '__main__':
         "https": "",
     }
 
-    entry_point = 'http://www.jbex.com/openapi'  # like: https://api.xxx.yyy/openapi/ where xxx.yyy is your base domain
+    entry_point = 'http://api.jbex.com/openapi'  # like: https://api.xxx.yyy/openapi/ where xxx.yyy is your base domain
     b = BrokerClient(entry_point, api_key='', secret='', proxies=proxies)
 
     print(b.time())
